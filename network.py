b6a083894874b1fba70c4f4cb7514194c506a247
ckx-sec <56886249+ckx-sec@users.noreply.github.com>
2021-06-30 22:55:46 +0800

    Add files via upload

---
+++ network.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from node import *
+
+
+class Network:
+    def __init__(self):
+        self.node = []
+        self.adjMatrix
+        self.type = []
+
+    def generate_network(self):
+        pass
+
+    def addNode(self, node: Node):
+        pass
+
+    def removeNode(self):
+        pass
+
+    def input_topic(self, topicList: list, initialNode: Node):
+        pass
+
+
+class DownNetwork(Network):
+    pass
+
+
+class UpNetwork(Network):
+    pass
